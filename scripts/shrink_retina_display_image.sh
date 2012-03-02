67876e759cc4f61109709f6852244b8a2c63c948
adam <adam@shopkick.com>
2012-03-02 14:33:46 -0800

    Add support for 1.5x image resizing.

--- scripts/shrink_retina_display_image.sh
+++ scripts/shrink_retina_display_image.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Requires imagemagick
+source lib.sh
 
+# Requires imagemagick
 for var in "$@"
 do
-	imagemagick $var -filter Box -resize 50% ${var/\@2x/}
+  var1x=${var/\@2x/}
+	convert $var -filter Box -resize 50% -background white $var1x
+  echo "wrote ${var1x}."
+
+  var1_5x=${var1x}
+  append_1_5x ${var1_5x}
+	convert $var -filter Box -resize 75% -background white $__file
+  echo "wrote $__file."
 done
